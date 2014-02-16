baeb6992d2d3520b9a125e3269ef5adad6f251b5
Alexandre Garnier <zigarn@gmail.com>
2014-02-16 19:13:23 +0100

    Add extra properties support

--- src/main/java/org/apache/catalina/startup/CatalinaProperties.java
+++ src/main/java/org/apache/catalina/startup/CatalinaProperties.java
@@ -23,6 +23,8 @@ import java.net.URL;
 import java.util.Enumeration;
 import java.util.Properties;
 
+import org.apache.catalina.Globals;
+
 
 /**
  * Utility class to read the bootstrap Catalina configuration.
@@ -105,6 +107,8 @@ public class CatalinaProperties {
             properties=new Properties();
         }
 
+        loadExtraProperties();
+
         // Register the properties as system properties
         Enumeration<?> enumeration = properties.propertyNames();
         while (enumeration.hasMoreElements()) {
@@ -122,6 +126,83 @@ public class CatalinaProperties {
      */
     private static String getConfigUrl() {
         return System.getProperty("catalina.config");
+    }
+
+
+    /**
+     * Load extra properties files.
+     */
+    private static void loadExtraProperties() {
+        String extraPropertiesURLs = properties.getProperty("catalina.extra.properties.urls");
+        if (extraPropertiesURLs != null) {
+            for (String extraPropertiesURL : extraPropertiesURLs.split(",")) {
+                InputStream is = null;
+                try {
+                    String configUrl = replace(extraPropertiesURL.trim());
+                    if (configUrl != null) {
+                        is = (new URL(configUrl)).openStream();
+                    }
+                } catch (Throwable t) {
+                    handleThrowable(t);
+                }
+                if (is != null) {
+                    try {
+                        properties.load(is);
+                        is.close();
+                    } catch (Throwable t) {
+                        handleThrowable(t);
+                        log.warn("Unable to load extra properties " + extraPropertiesURL);
+                    }
+                } else {
+                    log.warn("Unable to find extra properties " + extraPropertiesURL);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * System property replacement in the given string.
+     *
+     * @param str The original string
+     * @return the modified string
+     */
+    private static String replace(String str) {
+        // Copied from Bootstrap.replace().
+        String result = str;
+        int pos_start = str.indexOf("${");
+        if (pos_start >= 0) {
+            StringBuilder builder = new StringBuilder();
+            int pos_end = -1;
+            while (pos_start >= 0) {
+                builder.append(str, pos_end + 1, pos_start);
+                pos_end = str.indexOf('}', pos_start + 2);
+                if (pos_end < 0) {
+                    pos_end = pos_start - 1;
+                    break;
+                }
+                String propName = str.substring(pos_start + 2, pos_end);
+                String replacement;
+                if (propName.length() == 0) {
+                    replacement = null;
+                } else if (Globals.CATALINA_HOME_PROP.equals(propName)) {
+                    replacement = Bootstrap.getCatalinaHome();
+                } else if (Globals.CATALINA_BASE_PROP.equals(propName)) {
+                    replacement = Bootstrap.getCatalinaBase();
+                } else {
+                    replacement = System.getProperty(propName);
+                }
+                if (replacement != null) {
+                    builder.append(replacement);
+                } else {
+                    builder.append(str, pos_start, pos_end + 1);
+                }
+                pos_start = str.indexOf("${", pos_end + 1);
+            }
+            builder.append(str, pos_end + 1, str.length());
+            result = builder.toString();
+        }
+        return result;
     }
 
 
